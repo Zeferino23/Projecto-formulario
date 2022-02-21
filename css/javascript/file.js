588155571d23dece4c0e55780c94cd430502bdb9
zeferinoraúl <80109803+Zeferino23@users.noreply.github.com>
2022-02-21 21:46:05 +0100

    altearação

---
+++ css/javascript/file.js
@@ -0,0 +1,22 @@
+var p=window.document.getElementById("fprovi")
+
+switch (p) {
+    case "Bengo":
+        
+createElement()
+
+
+        break;
+
+
+        case "Benguela":
+
+
+
+
+        break;
+
+
+    default:
+        break;
+}
